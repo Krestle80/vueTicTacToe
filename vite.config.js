37189ba5c75fbc8f595c1df327871970db6a32c4
Kronk <kylec0217@gmail.com>
2022-05-21 21:56:53 -0400

    deployment fix

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,17 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('/vueTicTacToe/', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: '/vueTicTacToe/'
 })
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+// };
